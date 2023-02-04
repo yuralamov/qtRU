7a71c702d24bbfd0e337a52382708bf868c2426c
Yura Lamov <44276956+yuralamov@users.noreply.github.com>
2023-02-04 10:16:07 +0300

    Update qtcreator.bat

--- qtcreator.bat
+++ qtcreator.bat
@@ -1,6 +1,29 @@
 echo off
 echo "Start Qt Creator with MinGW64 environment"
-set PATH=D:\msys64\mingw64\bin;D:\msys64\usr\local\bin;D:\msys64\usr\bin;D:\msys64\bin;D:\msys64\usr\bin\site_perl;D:\msys64\usr\bin\vendor_perl;D:\msys64\usr\bin\core_perl;D:\myApp\Qt;D:\myApp\Qt\6.4.2\mingw_64;D:\myApp\Qt\6.4.2\mingw_64\bin;D:\myApp\Qt\6.4.2\mingw_64\phrasebooks;D:\myApp\Qt\6.4.2\mingw_64\translations;D:\myApp\Qt\6.4.2\mingw_64\include;D:\myApp\Qt\Tools\mingw730_32\bin;D:\myApp\Qt\Tools\mingw730_64\bin;D:\myApp\Qt\Tools\mingw810_32\bin;D:\myApp\Qt\Tools\mingw810_64\bin;D:\myApp\Qt\Tools\CMake_64\bin;D:\myApp\Qt\Tools\Conan;D:\myApp\Qt\Tools\mingw1120_64\bin;D:\myApp\Qt\Tools\mingw1120_64\x86_64-w64-mingw32\bin;D:\myApp\Qt\Tools\Ninja;D:\myApp\Qt\Tools\OpenSSL\Win_x64\bin;D:\myApp\Qt\Tools\OpenSSL\Win_x86\bin;D:\myApp\Qt\Tools\Qt3DStudio\bin;D:\myApp\Qt\Tools\QtCreator\bin;D:\myApp\Qt\Tools\QtDesignStudio\bin;D:\myApp\Qt\Tools\QtInstallerFramework\4.5\bin;D:\myApp\Qt\Tools\sdktool\bin;D:\myApp\Qt\Docs\Qt-6.4.2;%PATH%
+set PATH=^
+D:\msys64\mingw64\bin;^
+D:\msys64\usr\local\bin;^
+D:\msys64\usr\bin;^
+D:\msys64\bin;^D:\msys64\usr\bin\site_perl;^
+D:\msys64\usr\bin\vendor_perl;^
+D:\msys64\usr\bin\core_perl;^
+D:\Qt;^
+D:\Qt\6.4.2\mingw_64;^
+D:\Qt\6.4.2\mingw_64\bin;^
+D:\Qt\6.4.2\mingw_64\phrasebooks;^
+D:\Qt\6.4.2\mingw_64\translations;^
+D:\Qt\6.4.2\mingw_64\include;^
+D:\Qt\Tools\CMake_64\bin;^
+D:\Qt\Tools\Conan;^
+D:\Qt\Tools\Ninja;^
+D:\Qt\Tools\OpenSSL\Win_x64\bin;^
+D:\Qt\Tools\Qt3DStudio\bin;^
+D:\Qt\Tools\QtCreator\bin;^
+D:\Qt\Tools\QtDesignStudio\bin;^
+D:\Qt\Tools\QtInstallerFramework\4.5\bin;^
+D:\Qt\Tools\sdktool\bin;^
+D:\Qt\Docs\Qt-6.4.2;^
+%PATH%
 D:
 
 CD D:\myApp\Qt\Tools\QtCreator\bin\
